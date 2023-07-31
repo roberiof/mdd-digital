d27a41ce2ea3d5eae296a8759b2f4325465e91e0
roberiof <antonioroberiofilho8@gmail.com>
2023-07-31 13:15:21 -0300

    style: add AOS lib

--- src/components/AuxHomeSection.tsx
+++ src/components/AuxHomeSection.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import {useEffect} from 'react'
 import abstractWhite from '../assets/abstract-white.png'
 import employee from '../assets/employee.jpg'
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const AuxHomeSection = () => {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
-    <section className='bg-primary py-12 px-8 md:px-28 text-white min-h-screen flex flex-col justify-between gap-8 '>
-        <img src={abstractWhite} alt="" className='hidden lg:flex w-36'/>
-        <div className='flex flex-col lg:flex-row justify-between items-center gap-24'>
-            <img src={employee} alt="Worker of MDD Digital e Cia" className='w-5/6 w-96 h-96 object-cover'/>
-            <div className='flex flex-col gap-6'>
-                <h2> Parcerias que impulsionam seu negócio. </h2>
-                <p className='font-bold text-xl'>A MDD Digital é uma agência digital inovadora e orientada para resultados, focada em ajudar empresas de todos os tamanhos a otimizar suas estratégias de marketing online. Nossa missão é proporcionar aos nossos clientes uma presença digital poderosa, que aumente a visibilidade de suas marcas e impulsione o crescimento dos seus negócios.</p>
-            </div>
+    <section className='bg-primary flex justify-center'>
+        <div className='py-12 px-8 text-white min-h-screen flex flex-col justify-center xl:justify-between gap-8 w-5/6 max-w-screen-2xl'>
+          <img src={abstractWhite} alt="" className='hidden xl:flex w-36' data-aos="zoom-in"/>
+          <div className='flex flex-col xl:flex-row justify-between items-center gap-14'>
+              <img src={employee} alt="Worker of MDD Digital e Cia" className='w-96 h-96 object-cover' data-aos="fade-right" data-aos-duration="500"/>
+              <div className='flex flex-col gap-6 max-w-3xl' data-aos="fade-left" data-aos-duration="500">
+                  <h2> Parcerias que impulsionam seu negócio. </h2>
+                  <p className='font-bold text-xl text-justify'>A MDD Digital é uma agência digital inovadora e orientada para resultados, focada em ajudar empresas de todos os tamanhos a otimizar suas estratégias de marketing online. Nossa missão é proporcionar aos nossos clientes uma presença digital poderosa, que aumente a visibilidade de suas marcas e impulsione o crescimento dos seus negócios.</p>
+              </div>
+          </div>
         </div>
     </section>
   )
